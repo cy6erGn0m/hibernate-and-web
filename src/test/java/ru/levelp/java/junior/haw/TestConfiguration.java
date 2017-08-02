d439e101b090dca034fbe83ee823e462a217713b
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-08-02 14:15:31 +0300

    Exclude production configurations in tests

--- src/test/java/ru/levelp/java/junior/haw/TestConfiguration.java
+++ src/test/java/ru/levelp/java/junior/haw/TestConfiguration.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 @Configuration
-@ComponentScan(basePackages = "ru.levelp.java.junior.haw")
+@ComponentScan(basePackages = "ru.levelp.java.junior.haw", excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
 public class TestConfiguration {
     @Bean
     public SocialNetwork network() {
